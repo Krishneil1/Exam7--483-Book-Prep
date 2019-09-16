3e980f02d2cd3160ac3e709e901f563ee1f8b557
Krishneil <Krishneil1@gmail.com>
2019-09-16 21:26:24 +1000

    Chapter1 First Thread Listing Demo

---
+++ Chapter1/Chapter1/Chapter1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+            for(int i= 0; i <4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(0);
+            }
+            t.Join();
+            Console.WriteLine("Press any key to Exit!");
+            Console.ReadKey();
+        }
+        public static void ThreadMethod()
+        {
+            for(int i = 0; i < 10;i ++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+    }
+}
+/*
+ * Foreground threads can be used to keep an application alive. Only when all foreground threads end does the 
+ * common language runtime (CLR) shut down your application. Background threads are then terminated.
+*/
